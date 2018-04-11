472d09c1f31be4973fa0bde728bdeadaffa7e1a7
Viktor Ahlqvist <viktor@rxbc.se>
2018-04-11 23:18:09 +0200

    Is this the working version?

--- layouts/community/ergodox/swedish-vikahl/keymap.c
+++ layouts/community/ergodox/swedish-vikahl/keymap.c
@@ -18,57 +18,58 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   $    |   !  |  "   |  #   |  #   |  %   | LEFT |           | RIGHT|   &  |  /   |  (   |  )   |  =   |  ?     |
  * |        |   1  |  2 @ |  3 £ |  4 $ |  5   |      |           |      |   6  |  7 { |  8 [ |  9 ] |  0 } |  + \   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   Å  |   Ä  |   Ö  |   P  |   Y  |      |           |      |   F  |   G  |   C  |   R  |   L  |   ;    |
- * | Del    |   Q  |   W  |   E  |   R  |   T  |  Up  |           | Down |   Y  |   U  |   I  |   O  |   P  |   [    |
+ * |        |   Å  |   Ä  |   Ö  |   P  |   Y  |      |           |      |   F  |   G  |   C  |   R  |   L  |   ,    |
+ * | Del    |   Q  |   W  |   E  |   R  |   T  |  Up  |           | Down |   Y  |   U  |   I  |   O  |   P  |   Å    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |   A  |   O  |   E  |   U  |   I  |      |           |      |   D  |   H  |   T  |   N  |  S   |   -    |
- * | Esc    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   '    |
+ * | Esc    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  Ö   |   Ä    |
  * |--------+------+------+------+------+------|  Lt1 |           |  Win |------+------+------+------+------+--------|
- * | LShift |   .  |   Z  |   X  |   C  |   V  |   B  |           |      |   N  |   M  |      |      |/ Ctrl| RShift |
- * |        |   .  |   Q  |   J  |   K  |   X  |   B  |           |      |   M  |   W  |   V  |   Z  |
+ * | LShift |   Q  |   J  |   K  |   X  |   B  | MDIA |           |      |   M  |   W  |   V  |   Z  |      |        |
+ * |        |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   -  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | CTRL |  L1  | AltGr| Left | Right|                                       |  Up  | Down | AltGr  |   ]  | ~L1  |
+ *   | CTRL | AltGr|  Win | Left | Right|                                       | AltGr| Down |  Up  | Left | Right|
+ *   | (.)) | AltGr|      | Left | Right|                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,---------------.
- *                                        | Lt   | LGui |       | Alt  |Ctrl/Esc|
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
- *                                 `--------------------'       `----------------------'
+ *                                        ,---------------.       ,---------------.
+ *                                        | Alt  |Ctrl/Esc|       | Alt  |Ctrl/Esc|
+ *                                 ,------|------|--------|       |------+--------+------.
+ *                                 |      |      |  Home  |       | PgUp |        |      |
+ *                                 | Space|Backsp|--------|       |------|  Tab   |Enter |
+ *                                 |      |ace   |  End   |       | PgDn |        |      |
+ *                                 `----------------------'       `----------------------'
  */
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        NO_DLR,         KC_1,           KC_2,    KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_DEL,         KC_Q,           KC_W,    KC_E,   KC_R,   KC_T,   KC_UP,
-        KC_ESC,         KC_A,           KC_S,    KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,           KC_X,    KC_C,   KC_K,   KC_X,   TG(MDIA),
-        CTL_T(KC_NO),   MO(SYMB),       LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
-                                                          ALT_T(KC_APP), KC_LGUI,
+        NO_DLR,         KC_1,       KC_2,           KC_3,   KC_4,   KC_5,   KC_LEFT,
+        KC_DEL,         KC_Q,       KC_W,           KC_E,   KC_R,   KC_T,   KC_UP,
+        KC_ESC,         KC_A,       KC_S,           KC_D,   KC_F,   KC_G,
+        KC_LSFT,        KC_Z,       KC_X,           KC_C,   KC_V,   KC_B,   MO(MDIA),
+        CTL_T(KC_DOT),  KC_RALT,    KC_LWIN,        KC_LEFT,KC_RGHT,
+                                                          KC_LALT, CTL_T(KC_ESC),
                                                                          KC_HOME,
                                                         KC_SPC, KC_BSPC, KC_END,
         // right hand
-        KC_RGHT,        KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_BSLASH,
-        TG(1),          KC_F,   KC_G,   KC_C,   KC_R,   KC_L,             NO_AM,
-                        KC_D,   KC_H,   KC_T,   KC_N,   KC_S,             NO_AE,
-        TG(MDIA),       KC_B,   KC_M,   KC_W,   KC_V,   CTL_T(KC_Z),      KC_RSFT,
-                        KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC, KC_FN1,
+        KC_RGHT,        KC_6,   KC_7,    KC_8,    KC_9,    KC_0,     NO_PLUS,
+        KC_DOWN,        KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     NO_AA,
+                        KC_H,   KC_J,    KC_K,    KC_L,    NO_OSLH,  NO_AE,
+        KC_LWIN,        KC_N,   KC_M,    KC_COMM, KC_DOT,  NO_MINS,  KC_RSFT,
+                        NO_ALGR,KC_DOWN, KC_UP,   KC_LEFT, KC_RGHT,
         KC_LALT, CTL_T(KC_ESC),
         KC_PGUP,
         KC_PGDN, KC_TAB, KC_ENT
-    ),
+),
 /* Keymap 1: Symbol Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+ * |        |      |      |      |      |      |      |           |      |      |      |  /   |   *  |  -   |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   @  |   {  |   }  |   [  |   ]  |      |           |      |   <  |   7  |   8  |   9  |   *  |   F12  |
+ * |        |  F1  |  F2  |  F3  |  F4  |      |      |           |      |      |  7   |  8   |  9   |  +   |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   #  |   (  |   )  |   \  |   /  |------|           |------|   >  |   4  |   5  |   6  |   +  |        |
+ * |        |  F5  |  F6  |  F7  |  F8  |      |------|           |------|      |  4   |  5   |  6   |  +   |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      | UND  | CUT  | COP  | PAS  |      |           |      |   ?  |   1  |   2  |   3  |   %  |        |
+ * |        |  F9  |  F10 |  F11 |  F12 |      |      |           |      |      |  1   |  2   |  3   | Enter|        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |   .  |   0  |   =  |      |      |
+ *   |      |      |      |      |      |                                       |  0   | ,    | .    | Enter|      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -81,20 +82,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
        // left hand
-       M(0),      KC_F1,   KC_F2,    KC_F3,    KC_F4,     KC_F5,    KC_TRNS,
-       KC_TRNS,   NO_AT,   ALGR(KC_7),  ALGR(KC_0),  NO_LBRC,   NO_RBRC,  KC_TRNS,
-       KC_TRNS,   KC_HASH, NO_LPRN,  NO_RPRN,  ALGR(KC_MINS), NO_SLSH,
-       KC_TRNS,   KC_TRNS, LCTL(KC_Z),  LCTL(KC_X),  LCTL(KC_C),    LCTL(KC_V),  KC_TRNS,
-       KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_TRNS,
+       KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                        KC_TRNS,KC_TRNS,
                                                KC_TRNS,
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, NO_LESS,   KC_7,   KC_8,    KC_9,    KC_KP_ASTERISK, KC_F12,
-                LSFT(NO_LESS), KC_4,   KC_5,    KC_6,    KC_KP_PLUS, KC_TRNS,
-       KC_TRNS, NO_QUES, KC_1,   KC_2,    KC_3,    LSFT(KC_5), KC_TRNS,
-                         KC_DOT,KC_0,  LSFT(KC_0),    KC_TRNS,  KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_PSLS, KC_PAST, KC_PMNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,    KC_PPLS, KC_TRNS,
+                KC_TRNS, KC_4,    KC_5,    KC_6,    KC_PPLS, KC_TRNS,
+       RESET,   KC_TRNS, KC_1,    KC_2,    KC_3,    KC_PENT, KC_TRNS,
+                         KC_0,    KC_COMM, KC_DOT,  KC_PENT, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
@@ -106,17 +107,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      | MsUp |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      |      |      |      |      |        |
+ * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      | Play | Pause| Prev | Next |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      | Lclk | Rclk |      |      |           |      |      |VolDn |VolUp | Mute |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      | Lclk | Rclk |                                       |      |      |      |      |      |
+ *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
+ *                                 |LeClk |RiClk |------|       |------|      |      |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -125,16 +126,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                            KC_TRNS, KC_TRNS,
                                                     KC_TRNS,
-                                  KC_TRNS, KC_TRNS, KC_TRNS,
+                                  KC_BTN1, KC_BTN2, KC_TRNS,
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_MPLY, KC_MPLY, KC_MPRV, KC_MNXT, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS,
                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
